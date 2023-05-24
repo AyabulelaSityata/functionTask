c713d08745977c1f5856a3bebedba742cfd3a5ed
AyabulelaSityata <sityataayabulela@gmail.com>
2023-05-24 15:53:59 +0200

    task complete

---
+++ script/code.js
@@ -0,0 +1,18 @@
+function greet() {
+    console.log('Hello there, I am Aya');
+}
+greet();
+
+function multiply(numb1, numb2){
+    return numb1 * numb2;
+}
+
+console.log(multiply(10, 5));
+
+
+
+function tableOfFive(numb5) {
+    return numb5 * 5;
+}
+
+console.log(tableOfFive(4));
